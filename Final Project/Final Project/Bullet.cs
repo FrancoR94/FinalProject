7f6b1b5c525daf1e98d4937d884910bf51e020a8
Franco <francoriparimartin@gmail.com>
2021-07-20 00:07:14 -0300

    Setting up window game

---
+++ Final Project/Final Project/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Final_Project
+{
+    class Bullet
+    { 
+        private Texture texture;
+        private Sprite sprite;
+        private Vector2f position;
+        private float speed;
+        private Bullet bullet;
+        public Bullet(Vector2f position)
+        {
+            texture = new Texture("sprites/bullet.jpg"); 
+            sprite = new Sprite(texture); 
+            sprite.Scale = new Vector2f(0.05f, 0.05f);
+            position = new Vector2f(600.0f, 500.0f); 
+            sprite.Position = position;
+            speed = 200.0f;
+        }
+        public void Update()
+        {
+            position.X -= speed * (2.0f / (float)Game.FRAMERATE_LIMIT);
+            sprite.Position = position;     
+        }
+        public void Draw(RenderWindow window)
+        {
+            window.Draw(sprite);
+        }
+        public void Dispose()
+        {
+            sprite.Dispose(); 
+            texture.Dispose();
+        }
+        public Vector2f GetPosition() 
+        {
+            return position;
+        }
+    }
+}
---
+++ Final Project/Final Project/GamePlay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Window;
+using SFML.System;
+using SFML.Graphics;
+
+
+namespace Final_Project
+{
+    class GamePlay
+    {
+        private Player player;
+        private Background background;
+        public GamePlay()
+        {
+            background = new Background();
+            player = new Player();
+        }
+        public void Update()
+        {
+            player.Update();
+        }
+        public void Draw(RenderWindow window)
+        {
+            background.Draw(window);
+            player.Draw(window);
+        }
+    }
+}
